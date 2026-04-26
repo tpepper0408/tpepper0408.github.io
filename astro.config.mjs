dda31a41fc4cee0eb18cddb8deed3fa211bdfea5
Tim Pepper <tim.pepper04@gmail.com>
2026-04-26 20:35:12 +0100

    Comedy Blog post and Youtube component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://tpepper.dev',
   base: '/',
-  integrations: [tailwind(), sitemap()],
+  integrations: [mdx(), tailwind(), sitemap()],
   markdown: {
     shikiConfig: {
       theme: 'github-dark',
